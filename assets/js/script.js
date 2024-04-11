644a57080f5936138839872f844bb566ecf2e9b6
EvrettG <skyrift@tpg.com.au>
2024-04-11 21:17:53 +1000

    now possible to call dogstats via api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,43 @@
-// api details
+const myform = document.getElementById('myform')
 
-const name = 'golden retriever';
+
+myform.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    dogstats()
+    .then(data => {
+        console.log(data); // Log the data when the promise resolves
+    })
+    .catch(error => {
+        console.error('Error:', error.message);
+    });
+
+});
+
+// api details
+const breed = 'golden retriever';
 const apiKey = 'HvKs7iQXKzjn7CZzWg2qmA==wrsKN7snFLYJDUvM';
 
-fetch(`https://api.api-ninjas.com/v1/dogs?name=${name}`, {
-  method: 'GET',
-  headers: {
-    'X-Api-Key': apiKey,
-    'Content-Type': 'application/json'
-  }
-})
+function dogstats() {
+  return fetch(`https://api.api-ninjas.com/v1/dogs?name=${breed}`, {
+      method: 'GET',
+      headers: {
+          'X-Api-Key': apiKey,
+          'Content-Type': 'application/json'
+      }
+  })
   .then(response => {
-    if (!response.ok) {
-      throw new Error('Request failed');
-    }
-    return response.json();
+      if (!response.ok) {
+          throw new Error('Request failed');
+      }
+      return response.json();
   })
   .then(result => {
-    console.log(result);
+      console.log(result);
+      
+      return result; // Return the result for further processing if needed
   })
   .catch(error => {
-    console.error('Error:', error.message);
+      console.error('Error:', error.message);
   });
+}
