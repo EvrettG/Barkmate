8fafafe0ac44b502b65a654382b83dbec84bdeb3
EvrettG <skyrift@tpg.com.au>
2024-04-16 20:40:41 +1000

    Immages update from breed

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,44 +21,32 @@ const apiKey = 'HvKs7iQXKzjn7CZzWg2qmA==wrsKN7snFLYJDUvM';
 
 
 // loads the picture from dog ceo
-function dogPicture(){
-    return fetch(`https://api.api-ninjas.com/v1/dogs?name=${breed}`, {
-        method: 'GET',
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Request failed');
-        }
-        return response;
-    })
-    .then(result => {
-        console.log(result);
-        
-        return result; // Return the result for further processing if needed
-    })}
+async function dogPicture(breed) {
+    const response = await fetch(`https://dog.ceo/api/breed/${breed}/images/random/4`);
+    const dogpic = await response.json();
+    console.log(dogpic);
+    document.getElementById('image1').src = dogpic.message[0]
+    document.getElementById('image2').src = dogpic.message[1]
+    document.getElementById('image3').src = dogpic.message[2]
+    document.getElementById('image4').src = dogpic.message[3]
+}
+
+// function dogPicture1(breed){
+//     return fetch(`https://dog.ceo/api/breed/${breed}/images/random/3`, {
+//         method: 'GET',
+//     })
+//     .then(response => {
+//         if (!response.ok) {
+//             throw new Error('Request failed');
+//         }
+//         console.log(response)
+//         return response;
+//     })
+//     .then(result => {      
+//         return result; // Return the result for further processing if needed
+// })}
+
 const myform = document.getElementById("myform");
-
-test.addEventListener("submit", function (event) {
-  event.preventDefault();
-
-  dogstats()
-    .then((data) => {
-      console.log(data); // Log the data when the promise resolves
-
-    })
-    .catch((error) => {
-      console.error("Error:", error.message);
-    });
-
-  }
-
-
-
-);
-
-// api details
-
-
 
 function dogstats(breed) {
     
@@ -96,10 +84,12 @@ document.addEventListener("DOMContentLoaded", function () {
   const instances = M.Slider.init(elems, options);
 });
 
-function createdogCard(data){
+// Alters stats, and picture on the main page based on breed selected and parsed
+// through the dogstats function
+function createdogCard(data, breed){
+    // Code to be deleted later
     const breedName = data[0].name
     const averageLifeExpec = (data[0].max_life_expectancy +data[0].min_life_expectancy) / 2
-    console.log(averageLifeExpec)
     const barking = data[0].barking
     const coatLength = data[0].coat_length
     const drooling = [0].drooling
@@ -113,15 +103,19 @@ function createdogCard(data){
     const shedding = data[0].shedding
     const trainability = data[0].trainability
 
-    sheddingId.style.width = `${(shedding * 20)}%`
-    barkingId.style.width = `${(barking * 20)}%`
-    energyId.style.width = `${(energy * 20)}%`
-    protectivenessId.style.width = `${(protectiveness * 20)}%`
-    trainabilityId.style.width = `${(trainability * 20)}%`
-    groomingID.style.width = `${(grooming * 20)}%`
-    goodWDId.style.width = `${(goodDogs * 20)}%`
-    goodWCId.style.width = `${(goodKids * 20)}%`
-    goodWSId.style.width = `${(goodStangers * 20)}%`
+    sheddingId.style.width = `${(data[0].shedding * 20)}%`
+    barkingId.style.width = `${(data[0].barking * 20)}%`
+    energyId.style.width = `${(data[0].energy * 20)}%`
+    protectivenessId.style.width = `${(data[0].protectiveness * 20)}%`
+    trainabilityId.style.width = `${(data[0].trainability * 20)}%`
+    groomingID.style.width = `${(data[0].grooming * 20)}%`
+    goodWDId.style.width = `${(data[0].good_with_other_dogs * 20)}%`
+    goodWCId.style.width = `${(data[0].good_with_children * 20)}%`
+    goodWSId.style.width = `${(data[0].good_with_strangers * 20)}%`
+
+    console.log(breed)
+
+    dogPicture(breed)
 
 }
 
@@ -134,8 +128,7 @@ test.addEventListener('submit', function(event) {
     dogstats(breed)
     .then(data => {
         console.log(data);// Log the data when the promise resolves
-        console.log(data[0].barking);
-        createdogCard(data); 
+        createdogCard(data, breed); 
     })
     .catch(error => {
         console.error('Error:', error.message);
